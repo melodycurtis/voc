9adf8becb2e16a753292a2be321196c050c293e1
melodycurtis <melodycurtis123@gmail.com>
2020-07-30 13:54:19 +0100

    dash layout

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage LRS applications',
+  serviceName: 'Manage learner records applications',
 
   // Default port that prototype runs on
   port: '3000',
