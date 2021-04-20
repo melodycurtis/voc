bb9ebfcc7660bce1ff4e1662f8f86bc6e9c6b765
melodycurtis <melodycurtis123@gmail.com>
2021-04-20 10:10:53 +0100

    fix

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage learner records applications',
+  serviceName: 'Sharing pilot results',
 
   // Default port that prototype runs on
   port: '3000',
